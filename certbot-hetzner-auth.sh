f47a44b0bc2fc0a1ec1041f1953970e277b7b9c6
Max Albrecht <1@178.is>
2022-05-04 17:13:25 +0200

    it works with auto config from DNS

--- certbot-hetzner-auth.sh
+++ certbot-hetzner-auth.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
+set -eu
 
-token=$(cat /etc/hetzner-dns-token)
-#search_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
-search_name=kultr.de
-validation_domain=letsencrypt-test.kiste.li._validation.kultr.de
+# env vars from certbot:
+# the hostname that is is validated
+challenge_domain="$CERTBOT_DOMAIN"
+# the content of the validation challenge
+challenge_string="$CERTBOT_VALIDATION"
 
-zone_id=$(curl \
-        -H "Auth-API-Token: ${token}" \
-        "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
-        jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
+# our config, the API token for the Hetzner DNS API
+api_auth_token=$(cat /etc/hetzner-dns-token)
 
-curl -X "POST" "https://dns.hetzner.com/api/v1/records" \
-     -H 'Content-Type: application/json' \
-     -H "Auth-API-Token: ${token}" \
-     -d "{ \"value\": \"${CERTBOT_VALIDATION}\", \"ttl\": 300, \"type\": \"TXT\", \"name\": \"${validation_domain}.\", \"zone_id\": \"${zone_id}\" }" > /dev/null 2>/dev/null
+# check if validation for this hostname was delegated (with a CNAME record)
+challenge_cname="$(dig +noall +short "_acme-challenge.${CERTBOT_DOMAIN}" | sed 's/\.$//')"
+if test -n "$challenge_cname"; then challenge_domain="$challenge_cname"; fi
 
-# just make sure we sleep for a while (this should be a dig poll loop)
-sleep 30
+# find the id of the "zone" in the DNS console which we need to modify
+zone_domain="$challenge_domain"
+while test -z "$zone_id" ; do
+  zone_id=$(curl --silent -H "Auth-API-Token: ${api_auth_token}" \
+    "https://dns.hetzner.com/api/v1/zones?search_name=${zone_domain}" | \
+    jq ".\"zones\"[] | select(.name == \"${zone_domain}\") | .id" 2>/dev/null | tr -d '"')
+  # if not found yet, continue looking with 1 less sudomain
+  zone_domain="$(echo "$zone_domain" | cut -s -d'.' -f 2-)"
+  # fail when search is exhausted (no more subdomains to remove)
+  if test -z "$zone_domain"; then
+    echo "ERROR: could not find DNS zone!" >&2;
+    exit 1;
+  fi
+done
+
+# create the actual "challenge DNS record"
+curl --silent -X "POST" "https://dns.hetzner.com/api/v1/records" \
+  -H 'Content-Type: application/json' \
+  -H "Auth-API-Token: ${api_auth_token}" \
+  -d "{ \"value\": \"${challenge_string}\", \"ttl\": 300, \"type\": \"TXT\", \"name\": \"${challenge_domain}.\", \"zone_id\": \"${zone_id}\" }"
+
+# wait until we can observe the correct value in the DNS record
+printf "waiting for DNS record to be visible..." && \
+while ! test "\"${challenge_string}\"" = "$(dig -t txt +short "${challenge_domain}")"; do printf '.'; sleep 3; done && \
+echo " OK!"
--- certbot-hetzner-cleanup.sh
+++ certbot-hetzner-cleanup.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
+set -eu
 
-token=$(cat /etc/hetzner-dns-token)
-#search_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
-search_name=kultr.de
-validation_domain=letsencrypt-test.kiste.li._validation.kultr.de
+# env vars from certbot:
+# the hostname that is is validated
+challenge_domain="$CERTBOT_DOMAIN"
+# the content of the validation challenge
+challenge_string="$CERTBOT_VALIDATION"
 
-zone_id=$(curl \
-        -H "Auth-API-Token: ${token}" \
-        "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
-        jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
+# our config, the API token for the Hetzner DNS API
+api_auth_token=$(cat /etc/hetzner-dns-token)
 
-record_ids=$(curl \
-        -H "Auth-API-Token: $token" \
+# check if validation for this hostname was delegated (with a CNAME record)
+challenge_cname="$(dig +noall +short "_acme-challenge.${CERTBOT_DOMAIN}" | sed 's/\.$//')"
+if test -n "$challenge_cname"; then challenge_domain="$challenge_cname"; fi
+
+# find the id of the "zone" in the DNS console which we need to modify
+zone_domain="$challenge_domain"
+while test -z "$zone_id" ; do
+  echo "zone_id: ${zone_id}" # DEBUG
+  echo "zone_domain: ${zone_domain}" # DEBUG
+  zone_id=$(curl --silent -H "Auth-API-Token: ${api_auth_token}" \
+    "https://dns.hetzner.com/api/v1/zones?search_name=${zone_domain}" | \
+    jq ".\"zones\"[] | select(.name == \"${zone_domain}\") | .id" 2>/dev/null | tr -d '"')
+  # if not found yet, continue looking with 1 less sudomain
+  zone_domain="$(echo "$zone_domain" | cut -s -d'.' -f 2-)"
+  # fail when search is exhausted (no more subdomains to remove)
+  if test -z "$zone_domain"; then
+    echo "ERROR: could not find DNS zone!" >&2;
+    exit 1;
+  fi
+done
+
+# find and delete the validation record (possibly several)
+record_ids=$(curl --silent -H "Auth-API-Token: ${api_auth_token}" \
         "https://dns.hetzner.com/api/v1/records?zone_id=$zone_id" | \
-       jq ".\"records\"[] | select(.name == \"${validation_domain}.\") | .id" 2>/dev/null | tr -d '"')
+       jq ".\"records\"[] | select(.name == \"${challenge_domain}.\") | .id" 2>/dev/null | tr -d '"')
 
-for record_id in $record_ids
-do
-        curl -H "Auth-API-Token: $token" \
-                -X "DELETE" "https://dns.hetzner.com/api/v1/records/${record_id}" > /dev/null 2> /dev/null
+for record_id in $record_ids; do
+  curl --silent -H "Auth-API-Token: ${api_auth_token}" \
+    -X "DELETE" "https://dns.hetzner.com/api/v1/records/${record_id}"
 done
